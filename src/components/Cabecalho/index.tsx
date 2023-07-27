220adf2e263ab2f0a7ea9d0795426ba4f3690dcd
Joao Victor <51519605+joao2223@users.noreply.github.com>
2023-07-27 01:26:27 -0300

    Update index.tsx

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -3,12 +3,21 @@ import user from '../../assets/Cabecalho/user.svg'
 import eye from '../../assets/Cabecalho/eye.svg'
 import question from '../../assets/Cabecalho/question.svg'
 import desk from '../../assets/Cabecalho/desk.svg'
+import { useNavigate, } from 'react-router-dom'
 
 export default function Cabecalho() {
+    const navigate = useNavigate()
+
+    function redirecionarParaInicio() {
+        navigate('/')
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.icons}>
-                <img src={desk} alt="" />
+                <button className={styles.botao_icone} onClick={() => redirecionarParaInicio()}>
+                    <img src={desk} alt="" />
+                </button>
                 <img src={eye} alt="" />
                 <img src={question} alt="" />
                 <img src={user} alt="" />
