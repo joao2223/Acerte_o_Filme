3ad5fb92794b6ad766280957cf8ce344d8b51cab
Joao Victor <51519605+joao2223@users.noreply.github.com>
2023-07-27 20:52:41 -0300

    Update index.tsx

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -3,25 +3,61 @@ import user from '../../assets/Cabecalho/user.svg'
 import eye from '../../assets/Cabecalho/eye.svg'
 import question from '../../assets/Cabecalho/question.svg'
 import desk from '../../assets/Cabecalho/desk.svg'
+import image from '../../assets/Cabecalho/image.svg'
+import next from '../../assets/Cabecalho/next.svg'
 import { useNavigate, } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Cabecalho() {
     const navigate = useNavigate()
+    const [show, setShow] = useState(false)
+
 
     function redirecionarParaInicio() {
         navigate('/')
     }
+    const fecharModal = () => setShow(false);
+    const abrirModal = () => {
+        setShow(true);
+    }
 
     return (
-        <div className={styles.container}>
-            <div className={styles.icons}>
-                <button className={styles.botao_icone} onClick={() => redirecionarParaInicio()}>
-                    <img src={desk} alt="" />
-                </button>
-                <img src={eye} alt="" />
-                <img src={question} alt="" />
-                <img src={user} alt="" />
+        <>
+            <div className={styles.container}>
+
+                <div className={styles.icons}>
+                    <button className={styles.botao_icone} onClick={() => redirecionarParaInicio()}>
+                        <img src={desk} alt="" />
+                    </button>
+                    <button className={styles.botao_icone} onClick={() => abrirModal()}>
+                        <img src={question} alt="" />
+                    </button>
+                    <img src={user} alt="" />
+
+                </div>
+
             </div>
-        </div>
+            <div className={show ? styles.modal : styles.esconde}>
+                <div className={styles.cabecalho_modal}>
+                    <button onClick={() => fecharModal()} className={styles.botao_fecha}>X</button>
+                    <h1 className={styles.titulo_modal}>Como jogar?</h1>
+                </div>
+
+                <div>
+                    <div className={styles.informacao}>
+                        <img src={image} alt="" className={styles.imagem_info} />
+                        <p className={styles.texto_info}>Uma imagem irá aparecer para você tentar acertar</p>
+                    </div>
+                    <div className={styles.informacao}>
+                        <img src={eye} alt="" className={styles.imagem_info} />
+                        <p className={styles.texto_info}>Cada palpite errado vai exibir uma nova imagem</p>
+                    </div>
+                    <div className={styles.informacao}>
+                        <img src={next} alt="" className={styles.imagem_info} />
+                        <p className={styles.texto_info}>Se quiser pular a imagem, basta clicar em submit e deixar o campo em branco</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
