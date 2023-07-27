d6fa51960bab2670c41a8a7952a14026a5b5007b
Joao Victor <51519605+joao2223@users.noreply.github.com>
2023-07-27 01:59:29 -0300

    Update index.tsx

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -5,7 +5,7 @@ export default function Cards() {
     const navigate = useNavigate()
 
     function redirecionarParaFilmes(id: number) {
-        navigate(`/filme/${id}`, {state: {id}, replace:true})
+        navigate(`/filme/${id}`, { state: { id }, replace: true })
         console.log('1')
     }
 
@@ -13,9 +13,9 @@ export default function Cards() {
         <div className={styles.cards}>
             <button className={styles.card} onClick={() => redirecionarParaFilmes(1)}>1</button>
             <button className={styles.card} onClick={() => redirecionarParaFilmes(2)}>2</button>
-            <button className={styles.card}  onClick={() => redirecionarParaFilmes(3)}>3</button>
+            <button className={styles.card} onClick={() => redirecionarParaFilmes(3)}>3</button>
             <button className={styles.card} onClick={() => redirecionarParaFilmes(4)}>4</button>
-            <button className={styles.card}>5</button>
+            <button className={styles.card} onClick={() => redirecionarParaFilmes(5)}>5</button>
             <button className={styles.card}>6</button>
             <button className={styles.card}>7</button>
             <button className={styles.card}>8</button>
